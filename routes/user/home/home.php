9fedff7c1e0ce4ba48e30c04c651c0c34dc0fc27
remwina <jaikajeon@gmail.com>
2025-11-05 14:27:40 +0800

    changes

--- routes/user/home/home.php
+++ routes/user/home/home.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('user.home.index');
 })->name('home');
 
+Route::get('/gallery', function () {
+    return view('user.gallery.index');
+})->name('gallery');
+
+Route::get('/location', function () {
+    return view('user.location.index');
+})->name('location');
+
 Route::prefix('user')->group(function () {
     Route::get('/home', function () {
         return view('user.home.index');
