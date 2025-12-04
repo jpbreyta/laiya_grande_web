c6eaf64f8aeae12439bc6a82ffec60387f3c19a2
John Paul Bryan Reyta <149125392+jpbreyta@users.noreply.github.com>
2025-12-04 23:34:03 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$storagePath = '/tmp/storage';
+$bootstrapCache = '/tmp/bootstrap/cache';
+
+$dirs = [
+    $bootstrapCache,
+    $storagePath.'/framework/views',
+    $storagePath.'/framework/cache',
+    $storagePath.'/framework/sessions',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php', // <--- THIS WAS MISSING
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -18,4 +34,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    });
+
+$app->useStoragePath($storagePath);
+$app->instance('path.bootstrap', '/tmp/bootstrap');
+
+return $app->create();
