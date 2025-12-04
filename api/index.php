6d22a6984a74f3f1b6b140832cdddf9e32138ce9
John Paul Bryan Reyta <149125392+jpbreyta@users.noreply.github.com>
2025-12-04 23:17:40 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,29 +4,30 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
+// Force storage to /tmp
 $app->useStoragePath('/tmp/storage');
 
-$app->bootstrapPath = function () {
-    return '/tmp/bootstrap';
-};
+// Force bootstrap/cache to /tmp/bootstrap/cache
+define('LARAVEL_BOOTSTRAP_CACHE', '/tmp/bootstrap/cache');
 
 if (!is_dir('/tmp/bootstrap/cache')) {
     mkdir('/tmp/bootstrap/cache', 0777, true);
 }
-
 if (!is_dir('/tmp/storage/framework/views')) {
-    @mkdir('/tmp/storage/framework/views', 0777, true);
+    mkdir('/tmp/storage/framework/views', 0777, true);
 }
 if (!is_dir('/tmp/storage/framework/cache')) {
-    @mkdir('/tmp/storage/framework/cache', 0777, true);
+    mkdir('/tmp/storage/framework/cache', 0777, true);
 }
 if (!is_dir('/tmp/storage/framework/sessions')) {
-    @mkdir('/tmp/storage/framework/sessions', 0777, true);
+    mkdir('/tmp/storage/framework/sessions', 0777, true);
 }
+
+// Set the path for the PackageManifest early
+$app->useStoragePath('/tmp/storage');
+$app->useCachedPackagesPath(LARAVEL_BOOTSTRAP_CACHE);
 
 $request = Illuminate\Http\Request::capture();
 $response = $app->handle($request);
-
 $response->send();
-
 $app->terminate($request, $response);
