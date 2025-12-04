9734b8c0b6b5ef19c89b021b52463f8eeadab2e2
John Paul Bryan Reyta <149125392+jpbreyta@users.noreply.github.com>
2025-12-04 23:09:48 +0800

    Modify index.php for Laravel app on Vercel
    Updated the index.php to load the Laravel app and handle requests on Vercel.

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
 
-// Forward Vercel requests to Laravel's standard index.php
-require __DIR__ . '/../public/index.php';
+// 1. Loading the Laravel app
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// 2. FORCE Storage Path to /tmp (The only writable directory on Vercel)
+// This fixes the "Read-only file system" error for caches and views
+$app->useStoragePath('/tmp/storage');
+
+// 3. Handle the incoming request
+$request = Illuminate\Http\Request::capture();
+$response = $app->handle($request);
+
+$response->send();
+
+$app->terminate($request, $response);
